1e71f525489e980eb7e21df50e35f6c5574f8a7b
Tim Hsu <tim.hsu@tpisoftware.com>
2023-04-22 16:22:20 +0800

    addEmail page

--- vue-tailwindcss/src/main.js
+++ vue-tailwindcss/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 import "./style/style.css"; //在此引入
-import router from './router/index.js';
+import router from "./router/index.js";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
--- vue-tailwindcss/src/views/members/addEmail.vue
+++ vue-tailwindcss/src/views/members/addEmail.vue
@@ -6,12 +6,60 @@ export default {
       data() {
         return {};
       },
+      methods: {
+        back() {
+          this.$router.push('/members/addMember')
+        },
+        toNextPage() {
+          this.$router.push('/members/addContact');
+        },
+      },
     };
   },
 };
 </script>
 <template>
   <div>
-    <h2>addEmail component is working!</h2>
+    <button type="button" @click="back()" class="my-4">
+      <i class="fas fa-arrow-left fa-xl text-gray-700"></i>
+    </button>
+    <h2 class="text-yellow-900 text-3xl font-bold">輸入 Email</h2>
+    <form class="mt-10">
+      <div class="grid gap-4 lg:grid-cols-3">
+        <div class="w-full">
+          <p class="text-yellow-700 mb-2 text-xl">您的電子信箱</p>
+          <input
+            type="text"
+            class="w-full p-4 border border-yellow-600 outline-none rounded-lg"
+          />
+        </div>
+      </div>
+
+      <div class="w-full flex mt-2">
+        <input type="checkbox" />
+        <p class="text-yellow-700 ml-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+          ea iusto fugit magnam mollitia sunt tempore aut soluta cumque itaque!
+        </p>
+      </div>
+
+      <div class="w-full flex mt-2">
+        <input type="checkbox" />
+        <p class="text-yellow-700 ml-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+          ea iusto fugit magnam mollitia sunt tempore aut soluta cumque itaque!
+        </p>
+      </div>
+
+      <div class="text-right">
+        <button
+          type="button"
+          class="mt-4 px-32 py-3 rounded-md bg-yellow-900 text-white"
+          @click="toNextPage()"
+        >
+          下一步
+        </button>
+      </div>
+    </form>
   </div>
 </template>
