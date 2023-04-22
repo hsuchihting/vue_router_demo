866a503adcdc1cd9d8662492755f3c41f688c457
Tim Hsu <tim.hsu@tpisoftware.com>
2023-04-22 15:51:32 +0800

    addMember page layout

--- vue-tailwindcss/src/views/members/addMember.vue
+++ vue-tailwindcss/src/views/members/addMember.vue
@@ -6,6 +6,11 @@ export default {
       data() {
         return {};
       },
+      methods: {
+        back() {
+          this.$router.push({ name: "members" });
+        },
+      },
     };
   },
 };
@@ -13,6 +18,34 @@ export default {
 
 <template>
   <div>
-    <h2>addMember component is working!</h2>
+    <button type="button" @click="back()" class="my-4">
+      <i class="fas fa-arrow-left fa-xl text-gray-700"></i>
+    </button>
+    <h2 class="text-yellow-900 text-3xl font-bold">登入會員</h2>
+    <form class="mt-10">
+      <div class="grid gap-4 lg:grid-cols-3">
+        <div class="w-full">
+          <p class="text-yellow-700 mb-2 text-xl">帳號</p>
+          <input
+            type="text"
+            class="w-full p-4 border border-yellow-600 outline-none rounded-lg"
+          />
+        </div>
+      </div>
+      <div class="grid gap-4 lg:grid-cols-3 mt-6">
+        <div class="w-full">
+          <p class="text-yellow-700 mb-2 text-xl">密碼</p>
+          <input
+            type="text"
+            class="w-full p-4 border border-yellow-600 outline-none rounded-lg"
+          />
+        </div>
+      </div>
+
+      <router-link to="/members/addEmail" class="mt-2 block text-yellow-600 hover:underline">
+        註冊會員 
+        <i class="fas fa-chevron-right"></i>
+      </router-link>
+    </form>
   </div>
 </template>
